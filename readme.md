3fb8c15eda39196cb2c01e9e8a45d4502be3a087
scp1 <gideon.squarecap@gmail.com>
2016-06-06 08:33:31 -0700

    Setup Spring Project

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Power Plant Game
+
+Building a "board game" similar to a popular German game that has players using power plants to power cities across the country, powered by natural resources traded in a market.
+
+This is a pet project and I'm using it to learn Spring, AngularJS and WebSockets.
---
+++ src/main/java/com/pow/PowerPlantGameApplication.java
@@ -0,0 +1,12 @@
+package com.pow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerPlantGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerPlantGameApplication.class, args);
+	}
+}
